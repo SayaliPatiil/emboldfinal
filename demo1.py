27f5c92f5959001fb6ad18c1e943ec15dc35fa33
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-06 14:21:55 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,21 +1,3 @@
-"""Taking two numbers"""
-num1 = 1.5
-num2 = 6.3
-
-""" Add two numbers """
-addition = num1 + num2
-
-"""Display the addition"""
-print(f'The sum of {0} and {1} is {2}'.format(num1, num2, addition))
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-""" Add two numbers"""
-addition = float(num1) + float(num2)
-
-"""Display the sum"""
-print(f'The sum of {0} and {1} is {2}'.format(num1, num2,addition))
 
 # taking user input
 ch = input("Enter a character: ")
